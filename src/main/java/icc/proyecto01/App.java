5a7ee6a26dd0a998a85d73b87ecc5809e15e700d
Jose Martinez <jose.mtzb@yahoo.com.mx>
2012-04-16 01:13:46 -0500

    Cambios en App.java -Se prepara la insercion de un menu para realizar las operaciones. -Se agregan imports y se extiende un poco la documentacion.

--- src/main/java/icc/proyecto01/App.java
+++ src/main/java/icc/proyecto01/App.java
@@ -1,5 +1,9 @@
 package icc.proyecto01;
 
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+import java.io.IOException;
+
 /**
  * Aplicacion para el manejo de la venta
  * e inventario de una tienda de peliculas.
@@ -8,13 +12,22 @@ public class App {
 
     /**
      * El metodo <code>main</code> es el punto de
-     * arranque del programa.
+     * arranque del programa. Manda llamar al menu
+     * del programa y lo despliega hasta el termino
+     * de la ejecucion del programa.
      *
      * @param args a <code>String</code> Valores que
      * recibe desde la linea de ejecucion
      */
     public static void main(final String[] args) {
 
+        int opcion = 0;
+        BufferedReader entrada = new BufferedReader(new InputStreamReader(System.in));
+
+        //        Catalogo elCatalogo = new Catalogo();
+        MenuProg elMenu = new MenuProg();
+
+        // Elementos de prueba.
         Pelicula peli = new Pelicula();
         Pelicula p = new Pelicula();
 
@@ -23,7 +36,18 @@ public class App {
 
         peli.guardaInventario(p, "Toy Story", 150, 18);
         peli.muestraInventario(p);
+        // Terminan elementos de prueba.
 
-        System.out.println("Hello World!");
+        // Comentar esto para bloquear el menu.
+        while (opcion != -1) {
+            try {
+                opción = elMenu.muestraMenu(entrada, elCatalogo);
+            } catch (IOException e) {
+                System.out.println("Opción mal elegida, volver a intentarlo");
+                opcion = 0;
+            }
+        }
+        // Termina muestreo del menu
+
     }
 }
